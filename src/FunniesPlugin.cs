34c7121d75d1c8bbcb5db974fa3b33cf0df7cc23
Andrei <andreibelcristian@gmail.com>
2026-03-11 01:04:49 +0000

    Update FunniesPlugin.cs

--- src/FunniesPlugin.cs
+++ src/FunniesPlugin.cs
@@ -1,62 +1,30 @@
-﻿using System.Text.Json.Serialization;
 using CounterStrikeSharp.API.Core;
-using Funnies.Commands;
 using Funnies.Modules;
 
 namespace Funnies;
 
-public class FunniesConfig : BasePluginConfig
-{
-    [JsonPropertyName("ColorR")] public byte R { get; set; } = 171;
-    [JsonPropertyName("ColorG")] public byte G { get; set; } = 75;
-    [JsonPropertyName("ColorB")] public byte B { get; set; } = 209;
-    [JsonPropertyName("CommandPermission")] public string AdminPermission { get; set; } = "@css/generic";
-    [JsonPropertyName("RconPermission")] public string RconPermission { get; set; } = "@css/rcon";
-}
- 
-public class FunniesPlugin : BasePlugin, IPluginConfig<FunniesConfig>
+public class FunniesPlugin : BasePlugin
 {
     public override string ModuleName => "Funny plugin";
     public override string ModuleVersion => "0.0.1";
 
-    public FunniesConfig Config { get; set; }
-
     public override void Load(bool hotReload)
     {
-        Console.WriteLine("So funny :)");
+        Console.WriteLine("Wallhack plugin loaded");
 
         Globals.Plugin = this;
 
         RegisterListener<Listeners.CheckTransmit>(OnCheckTransmit);
-        RegisterListener<Listeners.OnTick>(OnTick);
 
-        AddCommand("css_money", "Gives a player money", CommandMoney.OnMoneyCommand);
-        AddCommand("css_rcon", "Runs a command", CommandRcon.OnRconCommand);
-
-        #if DEBUG
-        AddCommand("css_debug", "Debug command", CommandDebug.OnDebugCommand);
-        #endif
-
-        Invisible.Setup();
         Wallhack.Setup();
     }
 
     public override void Unload(bool hotReload)
     {
-        #if DEBUG
         if (hotReload)
         {
-            Invisible.Cleanup();
             Wallhack.Cleanup();
         }
-        #else
-        Console.WriteLine($"Reloading: hotReload? {hotReload}");
-        #endif
-    }
-
-    public void OnTick()
-    {
-        Invisible.OnTick();
     }
 
     public void OnCheckTransmit(CCheckTransmitInfoList infoList)
@@ -67,13 +35,6 @@ public class FunniesPlugin : BasePlugin, IPluginConfig<FunniesConfig>
                 continue;
 
             Wallhack.OnPlayerTransmit(info, player!);
-            Invisible.OnPlayerTransmit(info, player!);
         }
-    }
-
-    public void OnConfigParsed(FunniesConfig config)
-    {
-        Config = config;
-        Globals.Config = config;
     }
 }
